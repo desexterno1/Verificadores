fb3124ad6d9388ba622840a097e65f08b6b9cab4
desexterno1 <des_externo1@ccontrol.com.mx>
2016-10-05 12:28:39 -0500

    Update Verificador.java

--- src/verificador/Verificador.java
+++ src/verificador/Verificador.java
@@ -21,6 +21,7 @@ public class Verificador {
         // TODO code application logic here
         new presentacion.pantallaPrecio().setVisible(true);
         //new brach
+        asdasdasdasdasd
         
     }
     
